b3e5d3594135fc6a34a3234c31ca4114ad8b504f
denismitali17 <d.mitali1@alustudent.com>
2025-04-03 19:29:41 +0200

    EazyShip Rwanda

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f64d4eb6f13dcc0"; }
+/******/ 		__webpack_require__.h = function() { return "28ca352cca9a3f49"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
